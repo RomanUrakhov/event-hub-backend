3ca7ea5433d3400e8b263c3cbe537d7d1bf9edbd
RomanUrakhov <urahovroman@gmail.com>
2024-05-31 23:05:36 +0800

    refactor: add API prefix

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -19,10 +19,11 @@ def _register_blueprints(app: Flask):
     from .controllers.event import create_event_blueprint
     from .controllers.misc import create_misc_blueprint
 
+    # TODO: find the way to setup global API prefix at once and not duplicate for every blueprint
     event_bp = create_event_blueprint(event_repo=InMemoryEventRepository())
-    app.register_blueprint(event_bp)
+    app.register_blueprint(event_bp, url_prefix=app.config["APPLICATION_ROOT"])
 
     misc_bp = create_misc_blueprint()
-    app.register_blueprint(misc_bp)
+    app.register_blueprint(misc_bp, url_prefix=app.config["APPLICATION_ROOT"])
 
     return app
--- src/config.py
+++ src/config.py
@@ -6,6 +6,8 @@ load_dotenv()
 
 
 class Config:
+    APPLICATION_ROOT = "/api"
+
     TWITCH_CLIENT_ID = os.getenv("TWITCH_CLIENT_ID")
     TWITCH_CLIENT_SECRET = os.getenv("TWITCH_CLIENT_SECRET")
     TWITCH_REDIRECT_URI = os.getenv("TWITCH_REDIRECT_URI")
